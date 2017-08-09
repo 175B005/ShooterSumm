301787a353324d090f359a647b81ed46cd962112
dat17-05-ishiduki <175b005gp12@gmail.com>
2017-08-09 12:22:37 +0900

    シーン追

---
+++ Assets/scripts/TitleManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager: MonoBehaviour {
+    void Start()
+    {
+        GameParams.DrawScore();
+    }
+
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1")){	
+			SceneManager.LoadSceneAsync ("Play");
+	}
+}
+}
